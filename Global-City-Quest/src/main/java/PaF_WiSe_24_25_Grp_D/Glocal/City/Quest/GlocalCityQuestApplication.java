028b830e6d8977dfc6563ecd2fd14701fb81549b
timonew <timo.neuwerk@gmail.com>
2024-10-31 03:44:17 +0100

    KlassenRumpf

---
+++ Global-City-Quest/src/main/java/PaF_WiSe_24_25_Grp_D/Glocal/City/Quest/GlocalCityQuestApplication.java
@@ -0,0 +1,13 @@
+package PaF_WiSe_24_25_Grp_D.Glocal.City.Quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlocalCityQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlocalCityQuestApplication.class, args);
+	}
+
+}
---
+++ Global-City-Quest/src/test/java/PaF_WiSe_24_25_Grp_D/Glocal/City/Quest/GlocalCityQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package PaF_WiSe_24_25_Grp_D.Glocal.City.Quest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlocalCityQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
