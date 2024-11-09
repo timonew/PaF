472babf669b268b6217c1f12f8f3732189210c7b
timonew <timo.neuwerk@gmail.com>
2024-11-09 23:19:54 +0100

    j

---
+++ project/src/test/java/com/Paf_WiSe_24_25_GrpD/GlobalCityQuest/GlobalCityQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Paf_WiSe_24_25_GrpD.GlobalCityQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalCityQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
